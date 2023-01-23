54961d138e7517dd15e3cfdd09addfb662daa667
Angel <mercadomilletangel@gmail.com>
2023-01-22 23:12:25 -0800

    Challenge17 Basic Code Troubleshooting

--- Alta3 Challenges/Challenge17_Code_Repair.py
+++ Alta3 Challenges/Challenge17_Code_Repair.py
@@ -7,3 +7,4 @@ name= input("What is your name?\n")
 # This is what you should see when print runs-
 # Hi <name>! Welcome to Day 2 of Python Training!
 print(f"Hi {name.capitalize()}! Welcome to Day {mylist[1]} of {mylist[5]} Training!")
+
